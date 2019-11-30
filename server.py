83c45b5b8bb3167b27c3fd9078c2fe1c24671c7d
Kevin Shaw <kevin@Kevins-MacBook-Air.local>
2019-11-29 21:29:07 -0500

    fixing port problems hopefully

--- server.py
+++ server.py
@@ -4,9 +4,8 @@ from flask import Flask
 app = Flask(__name__)
 
 app.route('/')
-def index():
-    user_agent = request.headers.get('User-Agent')
     return '<p>Flask server is working</p>'
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=33507)
+
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
 
