4603103f90bc603385c9b8b62f9027807a0c0892
Kevin Shaw <kevin@Kevins-MacBook-Air.local>
2019-11-29 21:06:22 -0500

    testing for flask server

--- server.py
+++ server.py
@@ -3,5 +3,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.route('/')
+def index():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Flask server is working</p>'
 app.run(host= '0.0.0.0', port=environ.get('PORT'))
 
