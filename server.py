a18e39f2b68d3747cb268c80ea29d3199cc08242
Kevin Shaw <kevin@Kevins-MacBook-Air.local>
2019-11-29 21:35:39 -0500

    stupid syntax error :(

--- server.py
+++ server.py
@@ -3,9 +3,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.route('/')
+@app.route('/')
+def index():
     return '<p>Flask server is working</p>'
 
 port = int(os.environ.get('PORT', 5000))
+
 app.run(host='0.0.0.0', port=port)
 
