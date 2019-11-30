8feaebee98cd1bb5aa50629c149cc217a138d1e5
Kevin Shaw <kevin@Kevins-MacBook-Air.local>
2019-11-29 21:40:01 -0500

    ....

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def index():
     return '<p>Flask server is working</p>'
 
-port = int(os.environ.get('PORT', 5000))
+port = int(environ.get('PORT', 5000))
 
 app.run(host='0.0.0.0', port=port)
 
